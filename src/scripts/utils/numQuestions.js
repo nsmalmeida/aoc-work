ed28663fec0563a564dd8cdb53bf9785bda6a07f
Natã Almeida <n.smalmeida@outlook.com>
2025-01-20 01:54:27 -0300

    all questions

--- src/scripts/utils/numQuestions.js
+++ src/scripts/utils/numQuestions.js
@@ -1,7 +1,7 @@
 export const numQuestions = {
-    "green-theme": [0, 0],
-    "silver-theme": [0, 0],
+    "green-theme": [7, 8],
+    "silver-theme": [10, 10],
     "yellow-theme": [9, 9],
     "blue-theme": [20, 13],
-    "purple-theme": [0, 0]
+    "purple-theme": [8, 7],
 }
